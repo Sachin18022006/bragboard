081c86530a16f1ca51d262f818bcb5a71ad3ca2c
Sachin18022006 <sachinbssachinbs18@gmail.com>
2026-09-21 20:56:51 +0530

    Update backend API URL to new Render service

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -4,7 +4,7 @@ let detectedUrl =
   process.env.REACT_APP_API_BASE_URL;
 
 if (!detectedUrl && typeof window !== 'undefined' && window.location.hostname.includes('onrender.com')) {
-  detectedUrl = 'https://bragboard-backend-itvu.onrender.com';
+  detectedUrl = 'https://bragboard-79qp.onrender.com/docs';
 }
 
 let rawUrl = (detectedUrl || 'http://127.0.0.1:8000').replace(/\/api\/?$/, '');
